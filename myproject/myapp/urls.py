12fb118ca5db1855cd126b9155f338afff42fd2a
sandesh-personal <sandeshkoiralait@gmail.com>
2024-06-12 09:02:18 +0545

    dtl, bootstrap, url linking

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home_view, root_view_page, portfolio
+from .views import home_view, root_view_page, portfolio, learning_dtl_view, using_bootstrap_view
 
 urlpatterns = [
     path("home/", home_view),
-    path("", root_view_page),
+    path("", root_view_page,name="rootpage"),
     path("portfolio/", portfolio, name="portfolio"),
+    path("learning_dtl/", learning_dtl_view, name ="learning_dtl"),
+    path("using_bootstrap/", using_bootstrap_view, name ="using_bootstrap"),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -10,3 +10,36 @@ def root_view_page(request):
 
 def portfolio(request):
     return render(request, template_name= "portfolio/index.html")
+
+def home(request):
+    return render(request, template_name= "portfolio/index.html")
+
+def learning_dtl_view(request):
+    c = {"name": "Prixa Academy", "student": "jane"}
+    s = [
+        {"name": "jon", "age":30, "display": True},
+        {"name": "jon cena", "age":30, "display": False},
+        {"name": "jon abharm", "age": 30, "display": True},
+        {"name": "jonny ", "age": 30, "display": True},
+    ]
+    c.update(students=s)
+    return render(request, template_name= "myapp/dtl.html", context=c )
+
+
+
+
+# featurs from DTTL
+# 1. {% laod static %}
+# 2. {% url, "home" %}
+# 3. {% sattic "static/path %}
+# 4. {{name}}
+
+
+def using_bootstrap_view(request):
+    s =[
+        {"name": "jon", "age":30, "email": "aa@gmail.com", "address": "ktm", "display": True},
+        {"name": "jon cena", "age":30, "email": "aa@gmail.com", "address": "ktm", "display": False},
+        {"name": "jon abharm", "age": 30, "email": "aa@gmail.com", "address": "ktm", "display": True},
+        {"name": "jonny ", "age": 30, "email": "aa@gmail.com", "address": "ktm", "display": True},
+    ]
+    return render(request, template_name= "myapp/using_bootstrap.html", context= {"students": s})
